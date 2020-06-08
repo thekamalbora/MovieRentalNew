dbacf7cae9a7a32e3ef98fb07f506aa081fb3239
TheKamalBora <TheKamalBora@DESKTOP-L8O2BN4>
2020-06-08 23:18:24 +0530

    Add project files.

---
+++ MovieRental/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MovieRental.Startup))]
+namespace MovieRental
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
